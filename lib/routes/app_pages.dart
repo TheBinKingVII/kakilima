3b9f9682ad23e9eb8bc515aab1ef45ecca582a40
TheBinKingVII <bintoronob@gmail.com>
2025-12-06 22:30:01 +0700

    profile_page

--- lib/routes/app_pages.dart
+++ lib/routes/app_pages.dart
@@ -1,26 +1,20 @@
 import 'package:get/get.dart';
+import 'package:kakilima/features/auth/presentation/pages/login_page.dart';
+import 'package:kakilima/features/auth/presentation/pages/register_page.dart';
 import 'package:kakilima/screens/main_screen.dart';
 
 class AppPages {
   static const initial = Routes.main;
 
   static final routes = [
-    GetPage(
-      name: Routes.main,
-      page: () => MainScreen(),
-    ),
-    GetPage(
-      name: Routes.authLogin,
-      page: () => LoginPage(),
-    ),
-    GetPage(
-      name: Routes.authRegister,
-      page: () => RegisterPage(),
-    ),
+    GetPage(name: Routes.main, page: () => MainScreen()),
+    GetPage(name: Routes.authLogin, page: () => LoginPage()),
+    GetPage(name: Routes.authRegister, page: () => RegisterPage()),
   ];
-
 }
 
 class Routes {
   static const main = '/main';
+  static const authLogin = '/auth/login';
+  static const authRegister = '/auth/register';
 }
